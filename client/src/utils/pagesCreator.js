fcda7dd5ba57b67cc184acd9beebc8acb7664485
fscourge <fscourge@student.21-school.com>
2023-03-01 12:40:49 +0700

    max pages 6

--- client/src/utils/pagesCreator.js
+++ client/src/utils/pagesCreator.js
@@ -1,13 +1,13 @@
 export function createPages(pages, pagesCount, currentPage) {
-  if(pagesCount > 10) {
-    if(currentPage > 5) {
-      for (let i = currentPage-4; i <= currentPage+5; i++) {
+  if(pagesCount > 6) {
+    if(currentPage > 3) {
+      for (let i = currentPage - 2; i <= currentPage + 3; i++) {
         pages.push(i);
         if(i === pagesCount) break;
       }
     }
     else {
-      for (let i = 1; i <= 10; i++) {
+      for (let i = 1; i <= 6; i++) {
         pages.push(i);
         if(i === pagesCount) break;
       }
